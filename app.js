12a50c4ee7847058e588be0ef7eba657b1257bb3
ivan-adi-saputra <ivanadisaputra34@gmail.com>
2024-07-20 10:26:14 +0700

    update auth & cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 // router
 const categoriesRouter = require("./app/api/v1/categories/router");
@@ -21,6 +22,7 @@ const handleErrorMiddleware = require("./app/middlewares/handler-error");
 
 const app = express();
 
+app.use(cors()); // untuk mengizinkan url mengakses ke url backend
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
